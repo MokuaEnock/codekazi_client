04cba709d8839b633b6efd648fabf6a445312242
Enock <enock.mokua@student.moringaschool.com>
2023-03-26 22:49:03 +0300

    create header compoent and connect it to the backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header/header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/"/>
+        <Route path="/" />
       </Routes>
     </BrowserRouter>
   );
