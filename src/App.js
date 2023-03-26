6c74d8f5dc0500923e5d0996737ad0e89a2fb876
Enock <enock.mokua@student.moringaschool.com>
2023-03-27 00:41:54 +0300

    create basic structure of landing page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/footer";
 import Header from "./components/Header/header";
+import HomePage from "./pages/HomePage/homepage";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<HomePage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/pages/HomePage/homepage.js
+++ src/pages/HomePage/homepage.js
@@ -1,5 +1,22 @@
 import "./homepage.css";
+import { BiSearch } from "react-icons/bi";
 
 export default function HomePage() {
-  return <main id="home-page">This is the HomePage</main>;
+  return (
+    <main id="home-page">
+      <section id="home-page-1">
+        <h3>Welcome to jobless corner, where the idle meet.</h3>
+        <p>Find jobs in any field you desire.</p>
+        <form>
+          <input type="text" placeholder="Search job title or keyword" />
+          <button type="submit">
+            <BiSearch id="icon" />
+          </button>
+        </form>
+      </section>
+
+      <section id="home-page-2">1</section>
+      <section id="home-page-3">2</section>
+    </main>
+  );
 }
