b323f296d05cc9b60bb9ec41b8efcf4c9ab1b3c2
Enock <enock.mokua@student.moringaschool.com>
2023-03-26 23:46:30 +0300

    create display flex for root compeinnet and header structure

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
-  return <header>This is the header</header>;
+  return (
+    <header>
+      <nav>
+        <Link to="/">Jobs</Link>
+
+        <span>
+          <Link>Home</Link>
+          <Link>Jobs</Link>
+          <Link>Post</Link>
+        </span>
+        <Link></Link>
+      </nav>
+    </header>
+  );
 }
