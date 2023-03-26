8163a8ef08dc79a766a877aa637829e9a15613e8
Enock <enock.mokua@student.moringaschool.com>
2023-03-26 22:50:57 +0300

    create footer component and connect it to the app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer/footer";
 import Header from "./components/Header/header";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
