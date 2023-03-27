7cf4114e5b81e3144791fe109833fcae21ab29b5
Enock <enock.mokua@student.moringaschool.com>
2023-03-27 16:36:12 +0300

    create jobs header structure

--- src/pages/JobFormPage/jobformpage.js
+++ src/pages/JobFormPage/jobformpage.js
@@ -1,9 +1,24 @@
 import "./jobformpage.css";
 
+import { BiSearch } from "react-icons/bi";
+
 export default function Jobs() {
   return (
     <main id="job-page">
-      <section id="job-page-1"></section>
+      <section id="job-page-1">
+        <form id="search-jobs">
+          <input
+            type="text"
+            placeholder="Search for jobs, locations and description."
+          />
+          <button type="submit">
+            <BiSearch />
+          </button>
+        </form>
+        <div id="categories">
+          <span></span>
+        </div>
+      </section>
     </main>
   );
 }
