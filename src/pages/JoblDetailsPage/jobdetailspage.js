c7e4051bd9922965912f5ed1209912855ed63358
Enock <enock.mokua@student.moringaschool.com>
2023-03-28 04:31:01 +0300

    style the details page

--- src/pages/JoblDetailsPage/jobdetailspage.js
+++ src/pages/JoblDetailsPage/jobdetailspage.js
@@ -1,6 +1,7 @@
 import "./jobdetailspage.css";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { FaBuilding } from "react-icons/fa";
 
 export default function Detail() {
   let { id } = useParams();
@@ -24,12 +25,33 @@ export default function Detail() {
   }
   return (
     <main id="job-detail">
-      <h1>{job.title}</h1>
-      <p>{job.description}</p>
-      <p>{job.company}</p>
-      <p>{job.education_level}</p>
-      <p>{job.location}</p>
-      <p>{job.employment_type}</p>
+      <section id="job-detail-head">
+        <FaBuilding id="icon-b" />
+        <span>
+          <h3>{job.title}</h3>
+          <p>{job.company}</p>
+        </span>
+
+        <div>
+          <button>Delete</button>
+        </div>
+      </section>
+
+      <section id="job-detail-desc">
+        <h4>Description</h4>
+        <p>{job.description}</p>
+      </section>
+
+      <section id="job-detail-desc1">
+        <h4>Information</h4>
+        <div>
+          <span>{job.education_level}</span>
+          <span>{job.location}</span>
+          <span>{job.employment_type}</span>
+          <span>{job.salary_range}</span>
+          <span>{job.years_of_experience} years.</span>
+        </div>
+      </section>
     </main>
   );
 }
